8b247e926598381c30027580330121896b8af056
Peter L. Adams <36312695+pladams9@users.noreply.github.com>
2020-05-20 12:23:14 -0500

    Added code for FormulaParser.get_node_value()

--- src/model/formula_parser.py
+++ src/model/formula_parser.py
@@ -4,8 +4,31 @@ class FormulaParser:
 
     def update_nodes(self, nodes):
         for node in nodes:
-            self._nodes[node] = nodes[node]
+            self._nodes[node] = str(nodes[node])
 
     def delete_nodes(self, nodes):
         for node in nodes:
             self._nodes.pop(node)
+
+    def get_node_value(self, node):
+        formula = self._nodes[node]
+        if formula[0] == '=':
+            value = self._parse_formula(formula[1:])
+        else:
+            value = self._cast_value(formula)
+        return value
+
+    def _parse_formula(self, formula):
+        if formula[0] == '[':
+            return 22
+        else:
+            return 2
+
+    def _cast_value(self, value):
+        if value.replace('.', '').replace('-', '').isdigit():
+            if value.count('.') == 1:
+                return float(value)
+            else:
+                return int(value)
+        else:
+            return value
--- tests/model/test_formula_parser.py
+++ tests/model/test_formula_parser.py
@@ -12,14 +12,14 @@ class TestFormulaParser(unittest.TestCase):
         }
         self.cells_b = {
             (1, 1): 'TEST_2',
-            (5, 6): '=([3,1] * [2,4]) - [1,3]'
+            (5, 6): -5
         }
         self.cells_a_plus_b = {
             (1, 1): 'TEST_2',
             (1, 3): 24,
             (2, 4): '=1+1',
             (3, 1): '=[1, 3] - 2',
-            (5, 6): '=([3,1] * [2,4]) - [1,3]'
+            (5, 6): -5
         }
         self.delete_cells = [
             (1, 1),
@@ -29,31 +29,37 @@ class TestFormulaParser(unittest.TestCase):
             (2, 4): '=1+1',
             (3, 1): '=[1, 3] - 2'
         }
+        self.cells_a_values = {
+            (1, 1): 'Test',
+            (1, 3): 24,
+            (2, 4): 2,
+            (3, 1): 22
+        }
         self.parser = fp.FormulaParser()
 
     def test_update_nodes(self):
         self.parser.update_nodes(self.cells_a)
         self.assertEqual(self.parser._nodes.keys(), self.cells_a.keys())
         for cell in self.cells_a:
-            self.assertEqual(self.parser._nodes[cell], self.cells_a[cell])
+            self.assertEqual(self.parser._nodes[cell], str(self.cells_a[cell]))
 
         self.parser.update_nodes(self.cells_b)
         self.assertEqual(self.parser._nodes.keys(), self.cells_a_plus_b.keys())
         for cell in self.cells_a_plus_b:
-            self.assertEqual( self.parser._nodes[cell], self.cells_a_plus_b[cell])
+            self.assertEqual(self.parser._nodes[cell], str(self.cells_a_plus_b[cell]))
 
     def test_delete_nodes(self):
         self.parser.update_nodes(self.cells_a)
         self.parser.delete_nodes(self.delete_cells)
         self.assertEqual(self.parser._nodes.keys(), self.cells_a_minus_delete.keys())
         for cell in self.cells_a_minus_delete:
-            self.assertEqual(self.parser._nodes[cell], self.cells_a_minus_delete[cell])
+            self.assertEqual(self.parser._nodes[cell], str(self.cells_a_minus_delete[cell]))
 
-    def xtest_get_node_value(self):
+    def test_get_node_value(self):
         self.parser.update_nodes(self.cells_a)
         for cell in self.cells_a:
-            # TODO: check value
-            pass
+            with self.subTest(cell=cell, formula=self.cells_a[cell]):
+                self.assertEqual(self.parser.get_node_value(cell), self.cells_a_values[cell])
 
 
 if __name__ == '__main__':
