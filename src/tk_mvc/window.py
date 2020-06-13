b54957b1abd484a90173f013bfbdafb4276d6f0f
Peter L. Adams <36312695+pladams9@users.noreply.github.com>
2020-06-13 00:56:10 -0500

    Update window.py

--- src/tk_mvc/window.py
+++ src/tk_mvc/window.py
@@ -1,12 +1,15 @@
-import tkinter as tk
+from tkinter import Frame
+from tkinter import Toplevel
+from .view import View
 
 
-class BaseWindow(tk.Frame):
+class BaseWindow(Frame):
     """
     All windows in tk_mvc derive from BaseWindow. It is simply a Frame that View will place inside a TopLevel
     upon creation.
     """
 
-    def __init__(self, view, parent):
-        super().__init__(parent)
+    def __init__(self, view: View, parent_toplevel: Toplevel):
+        super().__init__(parent_toplevel)
         self._view = view
+        self._parent_toplevel = parent_toplevel
